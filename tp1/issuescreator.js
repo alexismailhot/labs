3e2bc938c3b02f495588c4c91137e180fc53eeec
Ian Létourneau <letourneau.ian@gmail.com>
2018-01-26 18:20:25 -0500

    Update issuecreator.js

--- tp1/issuescreator.js
+++ tp1/issuescreator.js
@@ -1,109 +1,65 @@
 var request = require("request");
 
 var teams = [
-    "glo2003/h17-team5",
-    // "glo2003/h17-team1",
-    // "glo2003/h17-team2",
-    // "glo2003/h17-team3",
-    // "glo2003/h17-team4",
-    // "glo2003/h17-team5",
-    // "glo2003/h17-team6",
-    // "glo2003/h17-team7",
-    // "glo2003/h17-team8",
-    // "glo2003/h17-team9",
-    // "glo2003/h17-team10",
-    // "glo2003/h17-team11",
-    // "glo2003/h17-team12",
-    // "glo2003/h17-team13",
-    // "glo2003/h17-team14",
-    // "glo2003/h17-team16",
-    // "glo2003/h17-team17",
-    // "glo2003/h17-team18",
+    "glo2003/glo-2003-h18-team01",
+    "glo2003/glo-2003-h18-team02",
+    "glo2003/glo-2003-h18-team03",
+    "glo2003/glo-2003-h18-team04",
+    "glo2003/glo-2003-h18-team05",
+    "glo2003/glo-2003-h18-team06",
+    "glo2003/glo-2003-h18-team07",
+    "glo2003/glo-2003-h18-team08",
+    "glo2003/glo-2003-h18-team09",
+    "glo2003/glo-2003-h18-team10",
+    "glo2003/glo-2003-h18-team11",
+    "glo2003/glo-2003-h18-team12",
+    "glo2003/glo-2003-h18-team13",
+    "glo2003/glo-2003-h18-team14",
+    "glo2003/glo-2003-h18-team15",
+    "glo2003/glo-2003-h18-team16",
+    "glo2003/glo-2003-h18-team17",
+    "glo2003/glo-2003-h18-team18"
 ];
 
-const i1body = `Using the github api (or the client available in your language),
-
-this project should retrieve the github projects available from the github
-token you'll have generated. The token should be injected in the software
-using an environment variable named \`GITHUB_TOKEN\`.
-
-For each contributor you should retrieve:
-
-- Login
-- ID
-- AvatarURL
-- URL
-- HTMLURL
-- Type
-- Number of contributions`;
-
-
-const i2body = `
-The api should return the Items retrieved in #1 at the URL \`/contributors\` using that format
+const i1body = `
+The api should return the schedule of the week at the URL \`/schedules/:startOfWeek\` using that format
 \`\`\`
-   {
-      "contributors":[
-         {
-            "login":"octocat",
-            "id":1,
-            "avatar_url":"https://github.com/images/error/octocat_happy.gif",
-            "url":"https://api.github.com/users/octocat",
-            "html_url":"https://github.com/octocat",
-            "type":"User",
-            "contributions":32
-         }
-      ]
-   }
-]\`\`\``;
-
-const i3body = `
-The api should return the pizza menu  at the URL \`/menu/pizzas\` using that format
-\`\`\`
-   {
-      "pizzas":[
+{
+  "days": [
+    {
+      "datetime": "2018-01-15T00:00:00.000Z",
+      "timeSlots": [
         {
-         "id": 1234,
-         "name": "Pep-fro",
-         "img": "/img/some-img-url.png",
-         "description": "Le grand classique à Gilles",
-         "ingredients": [
-            {
-                "id": 30,
-                "name": "Sauce tomate"
-            },
-            {
-                "id": 33,
-                "name": "Fromage"
-            }
-         ]
-        }
+          "datetime": "2018-01-15T11:00:00.000Z",
+          "scheduledStaffs": []
+        },
+        {
+          "datetime": "2018-01-15T12:00:00.000Z",
+          "scheduledStaffs": []
+        },
+        ...
+        {
+          "datetime": "2018-01-15T22:00:00.000Z",
+          "scheduledStaffs": []
+        },
       ]
-   }
+    },
+    ...
+  ]
+}
 \`\`\``;
 
-const i4body = `When doing a GET request at the root \`/\` of the server, it should return the index.html.
-All other static files (bundle.js, style.css, etc.) should be also configured to be access as static files.
-You can verify this by looking in the chrome devtools (F12 in chrome).
-`;
-
-
-const i5body = `
+const i2body = `
 Go look on the internet on how to document an http/json/rest api.
 developers.github.com is a good one.
 
 The API of this web service should be documented so the user interface can connect to it.
 `;
 
-const issues = [{"title": "Retrieve developers infos from github",
+const issues = [{"title": "Return the schedule of a week with your API",
                  "body": i1body},
-                {"title": "Add route to the API for developer infos",
-                 "body": i2body},
-                {"title": "Add route to the API for the pizza menu",
-                 "body": i3body},
-                {"title": "Add route to get the front end",
-                 "body": i4body},
                 {"title": "Document how to use your API",
-                 "body": i5body}];
+                 "body": i2body}];
 
 teams.forEach( function(team){
   issues.forEach( function(issue){
